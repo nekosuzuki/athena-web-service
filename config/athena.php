977882632b00c63b3582d95354356df12b40874d
nekosuzuki <70627497+nekosuzuki@users.noreply.github.com>
2020-09-28 15:40:53 +0000

    Update athena.php

--- config/athena.php
+++ config/athena.php
@@ -2,6 +2,6 @@
 
 return [
     'error_response' => ['Type' => 3],
-    'allowed_worlds' => explode(',', env('ATHENA_ALLOWED_WORLDS', 'rAthena')),
+    'allowed_worlds' => explode(',', env('ATHENA_ALLOWED_WORLDS', 'Neko')),
     'dump_requests' => env('ATHENA_DUMP_REQUESTS', false),
 ];
